3407bc7421640e006190d88c9c13014589f3ef43
Dao Duc Tam <ddt@gmail.com>
2026-04-25 22:32:20 +0700

    Initial commit: Simple Profile App with 2 activities and 2 fragments

---
+++ ADR58_DaoDucTam_Day2/app/src/main/java/com/tamdao/adr58_daoductam_day2/ProfileActivity.java
@@ -0,0 +1,47 @@
+package com.tamdao.adr58_daoductam_day2;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnBack = findViewById(R.id.btnBack);
+        Button btnInfo = findViewById(R.id.btnInfo);
+        Button btnAction = findViewById(R.id.btnAction);
+
+        btnBack.setOnClickListener(v -> finish());
+
+        // Default fragment
+        if (savedInstanceState == null) {
+            replaceFragment(new InfoFragment());
+        }
+
+        btnInfo.setOnClickListener(v -> replaceFragment(new InfoFragment()));
+        btnAction.setOnClickListener(v -> replaceFragment(new ActionFragment()));
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+}
